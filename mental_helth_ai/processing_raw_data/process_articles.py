67a075ab3416d4ddb82551802ad97746dcddef86
Marco Antônio <marco.netto@mjv.com.br>
2024-09-02 13:06:22 -0300

    feat: :sparkles: adicionando script para processar json dos artigos pegos com scrapping

--- mental_helth_ai/processing_raw_data/process_articles.py
+++ mental_helth_ai/processing_raw_data/process_articles.py
@@ -1,2 +1,19 @@
-# Processa os artigos encontrados (jsons e pdfs)
-# Salva-os em um arquivo JSON conforme estrutura do schema do Weaviate
+import requests
+import json
+
+LIMIT_ARTICLES = 100
+
+with open('data/raw/articles/articles_metadata.json', 'r') as file:
+    articles = json.load(file)
+    articles = articles[:LIMIT_ARTICLES]
+
+for article in articles:
+    pdf_link = article['pdf_url']
+    pdf_content = requests.get(pdf_link).content
+    title = f'{article['title'][:150]}...' if len(article['title']) > 150 else article['title']
+    cleaned_title = title.lower().replace('\n', '').replace(' ', '_').replace('/', '_')
+
+    with open(
+        f'data/raw/articles/scrapped/{cleaned_title}.pdf', 'wb'
+    ) as file:
+        file.write(pdf_content)
